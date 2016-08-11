ee7934d4c0b9e5d97a18744a2e17392d6696214d
johnshito <john.sanchez@arkix.com>
2016-08-11 17:31:29 -0500

    11 de agosto 2016 :: estilos y page

--- js/ax-main.js
+++ js/ax-main.js
@@ -3,10 +3,23 @@ $(document).ready(function(){
         $('#manual').animate({top: 0}, 'slow')
     })
     
-    $("#flipbook").turn({
-		width: 400,
-		height: 300,
-		autoCenter: true
+    var responsive=(window.innerWidth > 768)?'double':'single';
+    
+    $(".flipbook").turn({
+		width: 1240,
+		height: 840,
+		autoCenter: true,
+        display: responsive
 	})
     
+    $(".flipbook").bind("turning", function(event, page, view) {
+
+        switch (page) {
+            case 2:
+            $('.cont-title').delay(700).fadeIn(2000);
+            break;
+            
+        }
+    });
+
 })
