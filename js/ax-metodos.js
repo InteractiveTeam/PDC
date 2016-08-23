c5a6ef6663ec56e9707044f8d17abf47cb95b998
johnshito <john.sanchez@arkix.com>
2016-08-23 10:35:30 -0500

    13 de agosto: estilos

--- js/ax-metodos.js
+++ js/ax-metodos.js
@@ -15,14 +15,14 @@ var epmModule = (function($){
             actions = this.setting;
             this.validateUser();
             $(document).ready(function(){                
-                $('.ax-page-5').load('page.html .page-6');
-                $('.ax-page-6').load('page.html .page-7');
-                $('.ax-page-7').load('page.html .page-8');
-                $('.ax-page-8').load('page.html .page-9');
-                $('.ax-page-9').load('page.html .page-10');
-                $('.ax-page-10').load('page.html .page-11');
-                $('.ax-page-11').load('page.html .page-12');
-                $('.ax-page-12').load('page.html .page-13');
+                $('.ax-page-5').load('page.html .page-5');
+                $('.ax-page-6').load('page.html .page-6');
+                $('.ax-page-7').load('page.html .page-7');
+                $('.ax-page-8').load('page.html .page-8');
+                $('.ax-page-9').load('page.html .page-9');
+                $('.ax-page-10').load('page.html .page-10');
+                $('.ax-page-11').load('page.html .page-11');
+                $('.ax-page-12').load('page.html .page-12');
                 
                 $('.ax-page-20').load('page.html .page-20');
 //                $('.ax-page-21').load('page.html .page-21');
